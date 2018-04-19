67799dd130b20627300b098b771d31178c355ebc
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-19 15:16:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    puts DB[:conn].execute("SELECT * FROM students WHERE grade = 9")[0]
+    return DB[:conn].execute("SELECT * FROM students WHERE grade = 9")
   end
 
   def save
